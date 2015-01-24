58400d3e0ea4f40b741b1594c3b5b9edc98f3930
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-01-24 10:59:43 -0800

    more excercises

--- trial.py
+++ trial.py
@@ -1,15 +1,21 @@
+# # excercise 1
 # color = input("What is your favorite color? ")
-
 # if color == "brown":
 #   print(color + " is POOOP")
 # else:
 #   print("You look good in " + color)
-
+# # excercise 2
 # number_1 = input("First number ")
 # number_2 = input("Second number ")
 # number_3 = input("Third number ")
 # max_of_three = max(number_1, number_2,  number_3)
 # print (max_of_three)
-
-name = input("What is your name?")
-print(name.count(""))
+# # excercise 3
+# name = input("What is your name?")
+# print(name.count(""))
+# # excercise 4
+letter = input("Enter a letter ")
+if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+  print("true")
+else:
+  print("false")
