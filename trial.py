c65c03de0aa1416b9b43d168a0bd007d434c2cc0
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-01-23 20:56:32 -0800

    python 3 max

--- trial.py
+++ trial.py
@@ -4,3 +4,9 @@ if color == "brown":
   print(color + " is POOOP")
 else:
   print("You look good in " + color)
+number_1 = input("First number")
+number_2 = input("Second number")
+number_3 = input("Third number")
+dicks = max(number_1, number_2,  number_3)
+
+print (dicks)
