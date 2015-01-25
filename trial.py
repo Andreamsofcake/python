a67d39efe255164ac58411827bc273f608c9bb0b
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-01-25 15:15:21 -0800

    all excercise

--- trial.py
+++ trial.py
@@ -5,17 +5,17 @@ if color == "brown":
 else:
   print("You look good in {}".format(color))
 # # excercise 2
-# number_1 = input("First number ")
-# number_2 = input("Second number ")
-# number_3 = input("Third number ")
-# max_of_three = max(number_1, number_2,  number_3)
-# print (max_of_three)
+number_1 = input("First number ")
+number_2 = input("Second number ")
+number_3 = input("Third number ")
+max_of_three = max(number_1, number_2,  number_3)
+print (max_of_three)
 # # excercise 3
-# name = input("What is your name?")
-# print(name.count(""))
+name = input("What is your name?")
+print(name.count(""))
 # # excercise 4
-# letter = input("Enter a letter ")
-# if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
-#   print("true")
-# else:
-#   print("false")
+letter = input("Enter a letter ")
+if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+  print("vowel")
+else:
+  print("consonant")
